c1c613737da7eb12d60a8fd5de0dc37146efa47f
thautwarm <twshere@outlook.com>
2017-09-12 16:22:42 +0800

    finish EBNF self-examine

--- EBNFParser/Parser/SelfExamine.py
+++ EBNFParser/Parser/SelfExamine.py
@@ -6,10 +6,10 @@ Created on Tue Sep 12 12:16:26 2017
 @author: misakawa
 """
 from ..ObjectRegex.Node import   Liter, ast, recur, Seq, ELiter,handle_error, re
-Name   = Liter('[a-zA-Z_][a-zA-Z0-9]*', name = 'NAME')
+Name   = Liter('[a-zA-Z_][a-zA-Z0-9]*', name = 'Name')
 Number = Liter('\d+',name = 'Number')
 String = Liter("[R]{0,1}'[\w|\W]*?'", name = 'String')
-
+NEWLINE= Liter('\n', name = 'NEWLINE')
 
 L_Bracket= '\{|\}|\(|\)|\[|\]'
 LBB = ELiter('{', name = 'LBB')
@@ -19,11 +19,12 @@ RBB = ELiter('}', name = 'RBB')
 RB  = ELiter(']', name = 'RB')
 RP  = ELiter(')', name = 'RP')
 
+
 SeqStar = ELiter('*', name = 'SeqStar')
 SeqPlus = ELiter('+', name = 'SeqPlus')
 
 Def     = ELiter('::=', name = 'Def')
-OrSign  = ELiter('|',   name = 'OrSign')
+OrSign  = ELiter("|",   name = 'OrSign')
 
 #token = re.compile(
 #        '|'.join([
@@ -39,55 +40,98 @@ OrSign  = ELiter('|',   name = 'OrSign')
 
 compile_closure = globals()
 
-Or   = ast(compile_closure,
-           [recur('Atom'), Seq(compile_closure,
-                              [OrSign, recur('Atom')],
-                              atleast = 0,
-                              name = "('|' Atom)*")
-           ],
-           name = 'Or')
-Atom = ast(compile_closure, 
-           [Name],
-           [LP,recur('Atom'),RP, 
-                Seq(compile_closure, 
+#Chain = ast(compile_closure,
+#            [ Seq(compile_closure,
+#                  [recur('Or')],
+#                  atleast = 1,
+#                  name = "Or+")],
+#            name = 'Chain'
+#            )
+
+Expr   = ast(compile_closure,
+             [Seq(compile_closure,
+                 [recur('Or')],
+                 atleast = 0,
+                 name    = 'Or+')
+             ],
+           name = 'Expr')
+                  
+Or = ast(compile_closure,
+         [recur('AtomExpr'),
+          Seq(compile_closure,
+              [OrSign, recur('AtomExpr')],
+              atleast = 0,
+              name = "('|' AtomExpr)*")
+          ],
+         name = 'Or'
+        )
+         
+
+AtomExpr = ast(compile_closure,
+               [recur('Atom'), recur('Trailer')],
+               name = 'AtomExpr'
+               )
+
+Trailer = Seq(compile_closure, 
                     [SeqStar],
                     [SeqPlus],
                     [LBB,Seq(compile_closure,
                              [Number],
                              atleast = 1,
                              atmost  = 2,
-                             name    = 'Number{1,2}'
+                             name    = 'Number{1 2}'
                              ),
                          RBB],
                     atleast = 0,
-                    atmost  = 1,
-                    name = "['*'|'+']")],
-           [LB,recur('Atom'),RB],
+                    name = "Trailer")
+           
+           
+Atom = ast(compile_closure, 
+           [Name],
+           [String],
+           [LB, recur('Expr'),RB],
+           [LP, recur('Expr'),RP],
            name = 'Atom'
            )
+
+
+                        
+            
                 
 Equals = ast(compile_closure,
-             [Name, Def, recur('Or')],
-             name = 'Equals'
-        )
+              [Name, Def, recur('Expr')],
+              name = 'Equals')
     
 Stmt  = ast(compile_closure,
             [Seq(compile_closure,
-                [Equals],
+                [recur("SPLIT"),recur('Equals'),recur("SPLIT")],
                 name = '(Equals)*',
                 atleast = 0
                 )],
             name = 'Stmt')
                     
-                    
+            
+SPLIT = Seq(compile_closure, 
+                  [NEWLINE], 
+                   name    = 'NEWLINE*',
+                   atleast = 0)
+     
+SPLIT.compile
+SPLIT.has_recur = False             
 Atom.compile
 Atom.has_recur   =True
+AtomExpr.compile
+AtomExpr.has_recur= True
+Trailer.compile
+Trailer.has_recur=True
 Or.compile
 Or.has_recur     =True
 Equals.compile
 Equals.has_recur =True
 Stmt.compile
 Stmt.has_recur   =True
+Expr.compile
+Expr.has_recur   =True
  
 
         
--- test_selfexamine.py
+++ test_selfexamine.py
@@ -6,13 +6,13 @@ Created on Tue Sep 12 13:43:26 2017
 @author: misakawa
 """
 
-from EBNFParser.Parser.SelfExamine import token,Stmt,String,re,Equals
+from EBNFParser.Parser.SelfExamine import handle_error, token,Stmt,String,re,Equals
 from EBNFParser.Parser.SelfExamine import *
+
 with open('selfexamine.ebnf') as f:
     lex = f.read()
 tokens = token.findall(lex)
-
-s = Equals.match(tokens)
-print(s)
+res = handle_error(Stmt.match)(tokens, partial=False)
+print(res)
 
 
