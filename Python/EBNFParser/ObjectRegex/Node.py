1cd2f7f4d593476aa509fd0b0e03abd8da2bc069
thautwarm <twshere@outlook.com>
2017-09-14 16:21:39 +0800

    蜜汁重复加了一个库

--- Python/EBNFParser/ObjectRegex/Node.py
+++ Python/EBNFParser/ObjectRegex/Node.py
@@ -8,39 +8,64 @@ Created on Mon Sep 11 17:49:50 2017
 
 import re
 DEBUG = False
-class MetaInfo(dict):
+
+
+class MetaInfo:
     """
     Meta information when parsing.
+
+        `count` is a property of MetaInfo.
+        It shows that how many tokenized(words) have been parsed,
+            which could be used for
+                - Alerting.
+                - Eliminating left recursions.
+
+        `trace` is a property of MetaInfo.
+        It shows a trace of recursive BNF Nodes,
+            which could be used for
+                - Debugging.
+                - Eliminating left recursions.
+
+        `rdx` is a property of MetaInfo.
+        It shows how many lines have beeb parsed now.
+            which could be used for
+                - Alerting.
+                - Debugging.
+
     """
-    def __init__(self, count=0 , rdx=0, trace = None):
-        self['count'] = count
-        self['trace'] = trace if trace else []
-        self['rdx']   = rdx
-        self['history'] = []
-    
-    
+
+    def __init__(self, count=0, rdx=0, trace=None):
+        """
+
+        :rtype: MetaInfo
+        """
+        self.count   = count
+        self.trace   = trace if trace else []
+        self.rdx     = rdx
+        self.history = []
+
     @property
     def branch(self):
         """
         Save a record of parsing history in order to trace back. 
         """
-        self['history'].append((self.count, self.rdx, len(self.trace)))
+        self.history.append((self.count, self.rdx, len(self.trace)))
         return self
-    
+
     @property
     def rollback(self):
         """
         Trace back.
         """
         try:
-            count, rdx, length = self['history'].pop()
+            count, rdx, length =self.history.pop()
         except IndexError:
             return None
         self.count = count
-        self.rdx   = rdx
+        self.rdx = rdx
         self.trace = self.trace[:length]
         return self
-    
+
     @property
     def pull(self):
         """
@@ -48,101 +73,53 @@ class MetaInfo(dict):
         Pop a record in parsing history.
         """
         try:
-            self['history'].pop()
+            self.history.pop()
         except IndexError:
             raise Exception("pull no thing")
         return self
 
-    
 
-  
-    
-    @property
-    def count(self):
-        """
-        `count` is a property of MetaInfo.
-        It shows that how many tokenized(words) have been parsed, 
-            which could be used for
-                - Alerting.
-                - Eliminating left recursions.
-        """
-        return self['count']
-    
-    @count.setter
-    def count(self,v):
-        self['count'] = v
-        return self
-    
-    @property
-    def trace(self):
-        """
-        `trace` is a property of MetaInfo.
-        It shows a trace of recursive BNF Nodes,
-            which could be used for
-                - Debugging.
-                - Eliminating left recursions.
-        """
-        return self['trace']
-    
-    @trace.setter
-    def trace(self,v):
-        self['trace'] = v
-        return self
-    
-    @property
-    def rdx(self):
-        """
-        `rdx` is a property of MetaInfo.
-        It shows how many lines have beeb parsed now.
-            which could be used for
-                - Alerting.
-                - Debugging.
-        
-        """
-        return self['rdx']
-    
-    @rdx.setter
-    def rdx(self,v):
-        self['rdx'] = v
-        return self
-    
-    
+
 class simple_mode(str):
     """
     a `simple_mode` is a `str` which has a name. 
     """
-    def setName(self,name):
+    name = None
+    def setName(self, name):
         self.name = name
         return self
-    def __str__(self, i = 0):
+
+    def __str__(self, i=0):
         body = super(simple_mode, self).__str__()
-        body = f"['{body}']" if body != '\n' else r'[\n]'+'\n'+'  '*(i+1)
+        body = f"['{body}']" if body != '\n' else r'[\n]' + '\n' + '  ' * (i + 1)
         return f"{self.name} {body}"
-    
+
+
 class mode(list):
     """
     a `mode` is a `list` which has a name. 
     """
+    name = None
     def setName(self, name):
         self.name = name
         return self
-    def __str__(self, i = 0):
-        body  = " ".join([ item.__str__(i+1) for item in  self])
-        space = '  '*(i+1) 
-        return f"{self.name}[{body}]\n{space}"
-    
-    
-    
-def reMatch(x, make = lambda x:x, escape = False):
+
+    def __str__(self, i=0):
+        body = " ".join([item.__str__(i + 1) for item in self])
+        space = '  ' * (i + 1)
+        return f'{self.name}[{body}]\n{space}'
+
+
+def reMatch(x, escape=False):
     token_rule = re.escape(x) if escape else x
-    re_        = re.compile( token_rule )
-    
+    re_ = re.compile(token_rule)
     def _1(y):
         if not y: return None
         r = re_.match(y)
-        if not r : return None
+        if not r: return None
         a, b = r.span()
-        if a is not 0 : raise Exception('a is not 0')
+        if a is not 0:
+            raise Exception('Do not match string at the first character.')
         if b != len(y):
             return None
         return y
@@ -153,44 +130,48 @@ class Liter:
     """
     Literal which can be initialized by an escaped string. 
     """
-    def __init__(self, i, name = None):
+
+    def __init__(self, i, name=None):
         self.token_rule, self.f = reMatch(i)
         self.name = name
         self.has_recur = False
-    def match(self, objs, meta_info = None, partial = True):
+
+    def match(self, objs, meta_info=None, partial=True):
         if not meta_info: meta_info = MetaInfo()
-        
         left = len(objs) - meta_info.count;
         if not left: return None
         r = self.f(objs[meta_info.count])
         if r is None or (not partial and left != 1):
             return None
-        
-        if r == '\n':
+        if r is '\n':
             meta_info.rdx += 1
         meta_info.count += 1
         return simple_mode(r).setName(self.name)
-    
+
+
 class ELiter:
     """
     Literal which can be initialized by a raw string. 
     """
-    def __init__(self, i, name = None):
-        self.token_rule, self.f = reMatch(i, escape = True)
-        self.name = name
-        self.has_recur = False
-    def match(self, objs, meta_info = None, partial = True):
+
+    def __init__(self, i, name=None):
+        self.token_rule, self.f = reMatch(i, escape=True)
+        self.name               = name
+        self.has_recur          = False
+
+    def match(self, objs, meta_info=None, partial=True):
         if not meta_info: meta_info = MetaInfo()
         if not objs[meta_info.count:]: return None
         r = self.f(objs[meta_info.count])
         if r is None or (not partial and len(objs) != 1):
             return None
-        
+
         if r == '\n':
             meta_info.rdx += 1
         meta_info.count += 1
         return simple_mode(r).setName(self.name)
-    
+
+
 class recur:
     def __init__(self, name):
         self.name = name
@@ -199,16 +180,15 @@ class recur:
 def redef(self, *args, **kwargs):
     self.__init__(*args, **kwargs)
     return self
-    
 
-    
+
 def handle_error(func):
-    def _f(objs, meta_info = None, partial = True):
-        if not meta_info: 
+    def _f(objs, meta_info=None, partial=True):
+        if not meta_info:
             meta_info = MetaInfo()
-        if  meta_info.trace:
-            return func(objs, meta_info = meta_info, partial = partial)
-        res = func(objs, meta_info = meta_info, partial = partial)
+        if meta_info.trace:
+            return func(objs, meta_info=meta_info, partial=partial)
+        res = func(objs, meta_info=meta_info, partial=partial)
         if res is None:
             c = meta_info.count
             r = meta_info.rdx
@@ -218,22 +198,24 @@ def handle_error(func):
                     c += 1
                     continue
                 break
-            info = " ".join(objs[c:c+10])
-            if len(objs)>c+10:
+            info = " ".join(objs[c:c + 10])
+            if len(objs) > c + 10:
                 info += '...'
             raise SyntaxError(f'Syntax Error at row {r} <= {info}')
         return res
+
     return _f
 
+
 class ast:
-    def __init__(self, compile_closure, *ebnf, name = None):
-        self.name     = name
-        self.possibilities= []
+    def __init__(self, compile_closure, *ebnf, name=None):
+        self.name = name
+        self.possibilities = []
         self.has_recur = False
-        self.cache    = ebnf
+        self.cache = ebnf
         self.compile_closure = compile_closure
         self.compiled = False
-        
+
     @property
     def compile(self):
         if self.compiled: return self
@@ -253,19 +235,19 @@ class ast:
         del self.cache
         self.compiled = True
         return self
-    
-    def match(self, objs, meta_info = None, partial = True):
-        if not meta_info: 
+
+    def match(self, objs, meta_info=None, partial=True):
+        if not meta_info:
             meta_info = MetaInfo()
         if DEBUG:
             print(f'{self.name} WITH {meta_info.trace}')
-        res   = mode().setName(self.name)
+        res = mode().setName(self.name)
         for possible in self.possibilities:
             meta_info.branch
             for thing in possible:
                 history_i = (meta_info.count, thing.name)
                 if thing.has_recur:
-                    
+
                     if history_i in meta_info.trace:
                         print("Found L-R! Dealed!")
                         r = None
@@ -275,13 +257,13 @@ class ast:
                 else:
                     meta_info.trace.append(history_i)
                     r = thing.match(objs, meta_info=meta_info)
-                
+
                 if r is None:
                     # nexr possible
                     res.clear()
                     meta_info.rollback
                     break
-                
+
                 if isinstance(thing, Seq):
                     res.extend(r)
                 else:
@@ -306,18 +288,19 @@ class ast:
             print('RET\n')
         return None
 
-    
+
 class Seq(ast):
-    def __init__(self,compile_closure, *ebnf, name = None, atleast = 1, atmost = None):
-        super(Seq, self).__init__(compile_closure, *ebnf, name = name)
+    def __init__(self, compile_closure, *ebnf, name=None, atleast=1, atmost=None):
+        super(Seq, self).__init__(compile_closure, *ebnf, name=name)
         self.atleast = atleast
-        self.atmost  = atmost 
-    def match(self, objs, meta_info=None, partial = True):
-        if not meta_info: 
+        self.atmost = atmost
+
+    def match(self, objs, meta_info=None, partial=True):
+        if not meta_info:
             meta_info = MetaInfo()
         if DEBUG:
             print(f'{self.name} WITH {meta_info.trace}')
-        res     = mode().setName(self.name)
+        res = mode().setName(self.name)
         atleast = self.atleast
         if not objs[meta_info.count:]:
             if atleast is 0:
@@ -327,34 +310,26 @@ class Seq(ast):
         atmost = self.atmost
         idx = 0
         if atmost:
-            
+
             while True:
                 if idx >= atmost:
                     break
-                r = super(Seq, self).match(objs, meta_info = meta_info)
+                r = super(Seq, self).match(objs, meta_info=meta_info)
                 if r is None:
                     break
                 res.extend(r)
                 idx += 1
         else:
             while True:
-                r = super(Seq, self).match(objs, meta_info = meta_info)
+                r = super(Seq, self).match(objs, meta_info=meta_info)
                 if r is None:
                     break
                 res.extend(r)
-                idx+=1
+                idx += 1
         if DEBUG:
             print(f"{self.name} <= {r}")
         if idx < self.atleast:
             meta_info.rollback
-            return  None
+            return None
         meta_info.pull
         return res
-            
-            
-            
-            
-            
-            
-            
-            
