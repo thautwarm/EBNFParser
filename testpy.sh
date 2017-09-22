e223042f9b5d37c3df08ced7c44907ae357a7722
thautwarm <twshere@outlook.com>
2017-09-22 18:53:45 +0800

    lisp

--- testpy.sh
+++ testpy.sh
@@ -4,3 +4,4 @@ python ../tests/Python/test.py
 python ../tests/Python/test_bootstrap.py
 python ../tests/Python/test_expy.py
 python ../tests/Python/parser_gen.py
+python ../tests/Python/test_lisp.py
--- tests/Python/test_lisp.py
+++ tests/Python/test_lisp.py
@@ -1,11 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 12 13:43:26 2017
+
+@author: misakawa
+"""
 
 from Misakawa.Bootstrap.Parser import *
 from Misakawa.ObjectRegex.Node import MetaInfo, Ast
 from Misakawa.ErrorFamily import handle_error
 from Misakawa.Bootstrap.Ast import ast_for_stmts
 from Misakawa.Bootstrap.Compile import compile as bootstrap_comp
-with open('../bootstrap.ebnf') as f:
+with open('../tests/lisp.ebnf') as f:
         lex = f.read()
 s= bootstrap_comp(lex, '')
-with open('../parser_gen(bootstrap).py','w') as f:f.write(s)
+with open('../parser_gen(lisp).py','w') as f:f.write(s)
+
+
+
+
 
