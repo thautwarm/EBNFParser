a79016275aefc87deaf7693fe2dc96f3634341e8
thautwarm <twshere@outlook.com>
2018-03-14 22:52:32 +0800

    change `handle_error`'s too broad syntax error

--- Python/Ruikowa/ErrorFamily.py
+++ Python/Ruikowa/ErrorFamily.py
@@ -6,59 +6,68 @@ Created on Sat Oct 14 19:28:51 2017
 @author: misakawa
 """
 
-
 import warnings
+
+
 class ObjectUsageError(Exception):
     pass
 
+
 class CheckConditionError(Exception):
     pass
+
 
 class UnsolvedError(Exception):
     pass
 
 
+class DSLSyntaxError(SyntaxError):
+    pass
+
+
 def handle_error(parser):
-    func    = parser.match
-    history = (0, parser.name)
-    def _f(objs, meta = None, partial=True):
-        if not meta: raise CheckConditionError("Meta Information not defined yet!")
-        res  = func(objs, meta=meta)
+    func = parser.match
+
+    def _f(words, meta=None, partial=True):
+        if not meta:
+            raise CheckConditionError("Meta Information not defined yet!")
+        res = func(words, meta=meta)
         if res is None:
             c = meta.count
             r = meta.rdx
-            for ch in objs[c:]:
+            for ch in words[c:]:
                 if ch is '\n':
                     r += 1
                     c += 1
                     continue
                 break
-            info = " ".join(objs[c:c + 10])
-            if len(objs) > c + 10:
+            info = " ".join(words[c:c + 10])
+            if len(words) > c + 10:
                 info += '...'
-            raise SyntaxError('''
+            raise DSLSyntaxError('''
 Syntax Error at {filename} row {r}
    Error startswith :
 {info}
-'''.format(r = r, info = info, filename=meta.fileName))
+'''.format(r=r, info=info, filename=meta.fileName))
         else:
-            if not partial and len(objs) != meta.count:
+            if not partial and len(words) != meta.count:
                 warnings.warn("Parsing unfinished.")
                 c = meta.count
                 r = meta.rdx
-                for ch in objs[c:]:
+                for ch in words[c:]:
                     if ch is '\n':
                         r += 1
                         c += 1
                         continue
                     break
-                info = " ".join(objs[c:c + 10])
-                if len(objs) > c + 10:
+                info = " ".join(words[c:c + 10])
+                if len(words) > c + 10:
                     info += '...'
-                raise SyntaxError('''
+                raise DSLSyntaxError('''
 Syntax Error at row {r}
    Error startswith :
 {info}
 '''.format(r=r, info=info))
         return res
+
     return _f
